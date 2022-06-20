ff0e9c61b5b8572c86997494819a0848c8a50e2b
LucaPotenza <99011451+LucaPotenza@users.noreply.github.com>
2022-06-20 23:54:05 +0200

    add final

---
+++ miniPlayerData.js
@@ -0,0 +1,15 @@
+klynt.miniPlayerData = {
+    "downloadAppWording": "Download App",
+    "thanksForWatchingWording": "Thanks for watching!",
+    "launchAppWording": "Then Launch Project",
+    "resumePlaybackWording": "Resume playback?",
+    "fullscreenInfoWording": "This program will launch in fullscreen",
+    "yesWording": "Yes",
+    "title": "Progetto transmedia finale",
+    "thumbnail": "Player/css/editor/img/mini-player.jpg",
+    "noWording": "No",
+    "analyticsKey": "",
+    "redirectToMobileApp": "auto",
+    "url": "",
+    "description": ""
+}
