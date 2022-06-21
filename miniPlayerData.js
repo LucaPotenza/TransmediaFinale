915a8aa4cdf6cb959a38622aa83612fd41df4044
LucaPotenza <99011451+LucaPotenza@users.noreply.github.com>
2022-06-21 15:02:24 +0200

    reupload responsive

---
+++ miniPlayerData.js
@@ -0,0 +1,15 @@
+klynt.miniPlayerData = {
+    "thanksForWatchingWording": "Thanks for watching!",
+    "resumePlaybackWording": "Resume playback?",
+    "fullscreenInfoWording": "This program will launch in fullscreen",
+    "yesWording": "Yes",
+    "title": "Progetto transmedia finale",
+    "thumbnail": "Player/css/editor/img/mini-player.jpg",
+    "noWording": "No",
+    "analyticsKey": "",
+    "redirectToMobileApp": "auto",
+    "downloadAppWording": "Download App",
+    "url": "",
+    "launchAppWording": "Then Launch Project",
+    "description": ""
+}
